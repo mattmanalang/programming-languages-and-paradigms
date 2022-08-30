88ce28c4951365ddb19da4c846cde5fec01a2413
mattmanalang <matt.manalang@gmail.com>
2022-08-30 08:02:14 -0500

    Settings for build on macOS

--- hw0/topword.cpp
+++ hw0/topword.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+  // It would probably be a good idea to avoid recursively searching through a file. It appears that common practice is to use the .eof() flag
   cout << "Hello world!";
   return 0;
 }

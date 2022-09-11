dc00fafd37aaeeae1d854bbfbb5efa97f823a909
Matthew Manalang <matt.manalang@gmail.com>
2022-09-11 11:06:41 -0500

    Submitted version

---
+++ hw0/README.txt
@@ -0,0 +1,8 @@
+To run the file (assuming you are already inside the "hw0" directory),
+first compile topword.cpp using the command below:
+g++ topword.cpp -o topword
+
+then call the executable with arguments as such:
+./topword "input=testcasefile.txt;output=yourfilename.txt"
+
+Failure to include arguments will yield a message and exit the program.
--- hw0/topword.cpp
+++ hw0/topword.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <fstream>
 #include <string>
+#include <algorithm>
 #include <cctype>
 #include <map>
 #include "ArgumentManager.h" 
@@ -18,8 +19,22 @@ void checkNumberOfArgs(int argc) {
   }
 }
 
-bool isOnlyAlpha(string word) {
-  for (char character : word) {
+void checkInputFile(ifstream& file) {
+  if(!file.is_open()) {
+    cout << "Error opening input file" << endl;
+    exit(1);
+  }
+}
+
+void checkOutputFile(ofstream& file) {
+  if(!file.is_open()) {
+    cout << "Error opening output file" << endl;
+    exit(1);
+  }
+}
+
+bool isOnlyLetters(string word) {
+  for (char character : word) { // Loops through a word to see if all of it's contents are letters, and not numbers
     if (!isalpha(character)) {
       return false;
     }
@@ -27,36 +42,83 @@ bool isOnlyAlpha(string word) {
   return true;
 }
 
+string convertDelimitersToSpace(string text) {
+  string converted(text);
+  for (char& character : converted) { // Loops through the entire line of text and changes any non-alphanumeric characters to a white space for easier processing
+    if (!isalnum(character))
+      character = ' ';
+  }
+  return converted;
+}
+
+string convertToLowercase(string word) {
+  string lowercaseWord = word;
+  for_each(lowercaseWord.begin(), lowercaseWord.end(), [](char& character) { // Loops through the string, or word in this case, and converts it's characters to lowercase
+    character = tolower(character);
+  });
+  return lowercaseWord;
+}
+
+void processLine(map<string, int>& wordCountTable, string text) {
+  stringstream convertedText(convertDelimitersToSpace(text));
+  string singleWord;
+
+  while (convertedText >> singleWord) { // Loops, getting words from a string stream to be analyzed and counted
+    if (!isOnlyLetters(singleWord))
+      continue;
+    wordCountTable[convertToLowercase(singleWord)]++;
+  }
+}
+
 map<string, int> tallyWords(ifstream& inputFile) {
   map<string, int> wordCountTable;
-  string word;
-  
-  while (inputFile >> word) {
-    if (!isOnlyAlpha(word) || word.size() < 1) {
-      continue;
-    }
-    wordCountTable[word]++;
-  }
+  string textLine;
 
+  while (getline(inputFile, textLine)) { // Loops through the file, getting each line
+    processLine(wordCountTable, textLine);
+  }
   return wordCountTable;
+}
+
+map<string, int> processFile(string fileName) {
+  ifstream inputFile(fileName); // Input file may contain thousands of lines and even more words. Potentially up to the limits of the computer's memory
+  checkInputFile(inputFile);
+
+  auto result = tallyWords(inputFile);
+  inputFile.close();
+  return result;
+}
+
+int findHighestOccurrence(map<string, int> wordCountTable) {
+  int maximumOccurrence = 0;
+  for (pair<string, int> element : wordCountTable) { // Linear search through all entries in the map to find the highest value
+    if (element.second > maximumOccurrence)
+      maximumOccurrence = element.second;
+  }
+  return maximumOccurrence;
+}
+
+void outputTopWords(map<string, int> wordCountTable, string outputFileName) {
+  int highestCount = findHighestOccurrence(wordCountTable);
+  ofstream outputFile(outputFileName);
+  checkOutputFile(outputFile);
+  for (pair<string, int> element : wordCountTable) { // Loops through each pair in the map and outputs to file the ones that match the highest count
+    if (element.second == highestCount) {
+      outputFile << element.first << " " << element.second << endl;
+    }
+  }
+  outputFile.close(); // Unlike the input file, the output file is going to tend to be MUCH smaller since it only contains the word(s) with the highest frequency.
 }
 
 int main(int argc, char const *argv[])
 {
   checkNumberOfArgs(argc);
   ArgumentManager argManager(argc, argv);
-  const string inputFilename = argManager.get("input");
-  const string outputFilename = argManager.get("output");
+  const string inputFileName(argManager.get("input"));
+  const string outputFileName(argManager.get("output"));
 
-  ifstream inputFile(inputFilename);
-  // Idea: have a string stream to build the characters read in from the file because there are delimiters of different types.
-  // Idea 2: use a dictionary, map, etc. to store the words found as well as the number of times that they have occurred.
-  map<string , int> wordOccurrence = tallyWords(inputFile);
-
-  for (auto iter = wordOccurrence.cbegin(); iter != wordOccurrence.cend(); iter++) {
-    cout << iter->first << " " << iter->second << endl;
-  }
-
+  map<string , int, less<string>> wordOccurrence(processFile(inputFileName)); // less<string> sorts the map by the key (a string) in ascending order during creation
+  outputTopWords(wordOccurrence, outputFileName);
 
   return 0;
 }

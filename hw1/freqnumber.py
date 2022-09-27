d136e94eacd6215370641a9e7fa6c0d6ea4633fe
mattmanalang <matt.manalang@gmail.com>
2022-09-26 23:02:06 -0500

    Recursive file reading, basic argument parsing

--- hw1/freqnumber.py
+++ hw1/freqnumber.py
@@ -1,1 +1,21 @@
-print("Hello world!")
+from ast import arg
+import sys # Used for reading in the command-line arguments
+
+def processLine(line):
+  print(line.strip())
+
+# Recursive method of reading through the file
+def processFile(inFile):
+  lineRead = inFile.readline()
+  if (lineRead == ""):
+    print("Line read was empty")
+    return
+  processLine(lineRead)
+  processFile(inFile)
+
+# Testing command: python freqnumber.py 'k=1;input=tc1.txt;output=freqnums.txt'
+commandArgs = sys.argv[1].split(';') # Splits the argument string into the different parts
+print(commandArgs)
+test = list(map(lambda argument: argument.split('='), commandArgs)) # Returns a list/map from the first round of finding the arguments
+argIter = iter(test)
+print(next(argIter)[1])

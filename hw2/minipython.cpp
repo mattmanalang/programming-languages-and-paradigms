4a4dbc491584959c0a5cef1ab7bae26ea7a1821c
Matthew Manalang <matt.manalang@gmail.com>
2022-11-01 21:46:54 -0500

    Lambda function to read in file

--- hw2/minipython.cpp
+++ hw2/minipython.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 // This minipython interpreter will be run by executing ./minipython python-file-name.py
 
 using namespace std;
@@ -14,8 +17,23 @@ void checkNumberOfArgs(int argc) {
 int main(int argc, char const *argv[]) {
   // Check args
   checkNumberOfArgs(argc);
-  const string pythonFile = argv[1]; // Get the filename that was passed in
+  const string pythonFileName = argv[1]; // Get the filename that was passed in
+  
+  auto loadFile = [](string fileName) { // Lambda function to load the contents of the python file into a vector
+    ifstream pythonFile(fileName);
+    vector<string> instructions;
+    string line;
+    while (getline(pythonFile, line)) {
+      instructions.push_back(line);
+    }
+    pythonFile.close();
+    return instructions;
+  };
 
-  // Driver code
-  cout << pythonFile << endl;
+  vector<string> pyInstructions = loadFile(pythonFileName);
+  for (string line : pyInstructions) {
+    cout << line[3] << endl;
+  }
+  
+  return 0;
 }
